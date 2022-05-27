6b2a9fe61b8168dc4d83de637da335b4982594f3
Izolabella R. Geddis <iza.yy10@gmail.com>
2022-05-26 23:42:20 -0400

    un-a

--- kaia.Bot/Objects/Discord/Message Receivers/Implementations/Counter.cs
+++ kaia.Bot/Objects/Discord/Message Receivers/Implementations/Counter.cs
@@ -9,7 +9,7 @@ namespace Kaia.Bot.Objects.Discord.Message_Receivers.Implementations
 {
     internal class Counter : IMessageReceiver
     {
-        public string Name => "Counter"; // a
+        public string Name => "Counter";
 
         public Task<bool> CheckMessageValidityAsync(KaiaUser Author, SocketMessage Message)
         {

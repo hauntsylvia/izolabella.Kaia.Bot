20c893b824161e1663cb06d914f0e736b9cfeb65
Izolabella R. Geddis <iza.yy10@gmail.com>
2022-05-27 00:14:53 -0400

    using bracket issue

--- kaia.Bot/QoL/Usings.cs
+++ kaia.Bot/QoL/Usings.cs
@@ -1,5 +1,4 @@
-﻿
-global using Kaia.Bot.Objects.KaiaControllers;
+﻿global using Kaia.Bot.Objects.KaiaControllers;
 global using Kaia.Bot.Objects.Constants;
 global using Kaia.Bot.Objects.Discord.Embeds.Bases;
 global using Newtonsoft.Json;

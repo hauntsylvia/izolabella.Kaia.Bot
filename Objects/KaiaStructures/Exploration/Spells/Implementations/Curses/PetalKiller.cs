c4a99a7feef3ff9cea681b73ad9420fa7fcbf6c7
Izolabella R. Geddis <iza.yy10@gmail.com>
2022-07-01 21:43:30 -0400

    petal killer

---
+++ Objects/KaiaStructures/Exploration/Spells/Implementations/Curses/PetalKiller.cs
@@ -0,0 +1,26 @@
+﻿using Kaia.Bot.Objects.KaiaStructures.Exploration.Spells.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaia.Bot.Objects.KaiaStructures.Exploration.Spells.Implementations.Curses
+{
+    public class PetalKiller : Spell
+    {
+        public PetalKiller() : base($"{Strings.Economy.CurrencyName} Killer",
+                                    $"Halves your {Strings.Economy.CurrencyName}.",
+                                    true,
+                                    Emotes.Counting.Curses,
+                                    new(DateTime.UtcNow, 7120222142))
+        {
+        }
+
+        public override Task ApplyAsync(SpellsProcessor From, KaiaUser ApplyTo)
+        {
+            ApplyTo.Settings.Inventory.Petals /= 2;
+            return Task.CompletedTask;
+        }
+    }
+}
